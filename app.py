2ca0b80843a41c72917d0b13fe966e0b0ab33eba
Diego Inácio <diegodci@gmail.com>
2019-12-30 14:57:40 -0300

    Update app
    Include form request (contact) and show mailing list

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, render_template, g
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -31,10 +31,26 @@ def about():
     PEOPLE = [{e[0]: row[i] for i, e in enumerate(cursor.description)} for row in rows]
     return render_template(f'{PAGE}.html', TITLE=TITLE, MENU=MENU, PEOPLE=PEOPLE)
 
-@app.route('/contact.html')
+@app.route('/contact.html', methods=['POST', 'GET'])
 def contact():
+    if request.method == 'POST':
+        fName = request.form.get('fullname')
+        email = request.form.get('email')
+        # Database
+        db_connect = sqlite3.connect('database/people.db')
+        cursor = db_connect.cursor()
+        cursor.execute(f'INSERT INTO maillist (fullname, email) VALUES ("{fName}", "{email}")')
+        db_connect.commit()
     TITLE, PAGE = MENU[2]['title'], MENU[2]['page']
     return render_template(f'{PAGE}.html', TITLE=TITLE, MENU=MENU)
+
+@app.route('/show')
+def show_mailing_list():
+    db_connect = sqlite3.connect('database/people.db')
+    cursor = db_connect.cursor()
+    query = cursor.execute('SELECT * FROM maillist;')
+    rows = query.fetchall()
+    return '<br>'.join(f'{row[0]} - {row[1]} ({row[2]})' for row in rows)
 
 
 if __name__ == '__main__':
